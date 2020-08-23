d8b942052733bf9722f29170669050a68cf17ea9
Tyrease Teer <kayaneseera@gmail.com>
2020-08-22 22:35:40 -0700

    Initial Commit

---
+++ Assets/Scripts/OpeningPath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningPath : MonoBehaviour
+{
+
+    // A component used to specify what paths are open
+    // This is used to connect layout types
+    // For example, if we have a layout with a left opening, we need to
+    // enable a layout with a right opening, and thus connect the two
+    // based on how much from the x or y position it has to be enabled
+    // relatived to the enabled layout.
+
+    [SerializeField]
+    private bool leftOpen, rightOpen, topOpen, bottomOpen;
+
+    [SerializeField]
+    private float xUnit, yUnity;
+}
---
+++ Assets/Scripts/ProceduralGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerator : MonoBehaviour
+{
+    //All Environmental Layers
+    public GameObject[] environmentalLayoutPrefabs;
+
+    const string ENVIRONMENT_LAYOUT_TAG = "EnvironmentLayout";
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetAllEnvironmentalLayouts();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Get's All Layouts. Has to be child of this object to work.
+    /// </summary>
+    void GetAllEnvironmentalLayouts()
+    {
+        GameObject[] foundChildren = transform.parent.GetComponentsInChildren<GameObject>();
+
+        //Check if under Environmental Tag
+        foreach(GameObject child in foundChildren)
+        {
+            //If even 1 child doesn't match the tag, get out of the function
+            if (!child.CompareTag(ENVIRONMENT_LAYOUT_TAG)) return;        
+        }
+
+        environmentalLayoutPrefabs = foundChildren;
+    }
+}
